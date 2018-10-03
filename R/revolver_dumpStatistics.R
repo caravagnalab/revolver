a9a1870aa96835b32c6c43c59e238b84578ca689
ttriche <tim.triche@gmail.com>
2018-10-03 11:04:13 -0400

    tidy up write_xlsx output, remove dependency on rJava

--- R/revolver_dumpStatistics.R
+++ R/revolver_dumpStatistics.R
@@ -16,38 +16,44 @@
 #' @export
 revolver_dumpStatistics <- function(x, file='REVOLVER-Statistics.xlsx') {
 
-  sheets <- list()
   pioHdr('REVOLVER: Dump statistics to Excel', "File: ", file, suffix='\t')
   pioTit('Features: drivers, occurrences & information transfer')
   ft <- revolver.featureMatrix(x)
 
-  sheets[["Drivers"]] <- x$dataset[x$dataset$is.driver,]
-  sheets[["Occurrences"]] <- as.data.frame(ft$occurrences)
+  # helper fn for data.frame'ing
+  dfWithRownames <- function(x) {
+    if (is(x, "matrix")) return(cbind(Name=rownames(x), as.data.frame(x)))
+    return(cbind(Name=names(x), Value=x))
+  }
+
+  sheets <- list()
+  sheets[["Drivers"]] <- x$dataset[x$dataset$is.driver, -1] # wtf is Misc?
+  sheets[["Occurrences"]] <- dfWithRownames(ft$occurrences)
   sheets[["Information_Transfer"]] <- ft$consensus.explosion
 
   if (!is.null(x$fit)) {
     pioTit('Fit: multinomial, penalty & fit')
-    sheets[["Multinomial"]] <- as.data.frame(x$fit$multinomial.penalty)
-    sheets[["Penalty"]] <- data.frame(Penalty=x$fit$penalty)
-    sheets[["Fit"]] <- data.frame(Fit=x$fit$solutionID)
+    sheets[["Multinomial"]] <- dfWithRownames(x$fit$multinomial.penalty)
+    sheets[["Penalty"]] <- dfWithRownames(x$fit$penalty)
+    sheets[["Fit"]] <- dfWithRownames(Fit=x$fit$solutionID)
   }
 
   if (!is.null(x$cluster)) {
     pioTit('Clustering: distance, clusters & features per cluster')
-    sheets[["Distance"]] <- data.frame(Distance=x$cluster$distances)
-    sheets[["Clusters"]] <- data.frame(Clusters=x$cluster$clusters)
+    sheets[["Distance"]] <- dfWithRownames(x$cluster$distances)
+    sheets[["Clusters"]] <- dfWithRownames(x$cluster$clusters)
     groups <- sort(x$cluster$clusters)
     for (g in sort(names(x$cluster$labels.colors))) {
       pts <- names(groups[groups==as.character(g)])
       cexp <- revolver.featureMatrix(x, patients=pts)$consensus.explosion
-      sheets[[g]] <- as.data.frame(cexp)
+      sheets[[g]] <- dfWithRownames(cexp)
     }
   }
 
   if (!is.null(x$jackknife)) {
     pioTit('Jackknife: co-clustering, clusters & features per cluster')
-    sheets[["Jackknife-clustering"]] <- data.frame(Cluster=x$jackknife$cluster)
-    sheets[["Jackknife-edges-detection"]] <- data.frame(Edges=x$jackknife$edges)
+    sheets[["Jackknife-clustering"]] <- dfWithRownames(x$jackknife$cluster)
+    sheets[["Jackknife-edges-detection"]] <- dfWithRownames(x$jackknife$edges)
   }
 
   write_xlsx(sheets, path=file) 
