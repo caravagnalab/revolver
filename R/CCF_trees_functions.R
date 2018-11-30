36f679f001dd068652f7a9c199751037971d2847
caravagn <gcaravagn@gmail.com>
2018-11-30 14:35:50 +0000

    Layout reshaped

--- R/CCF_trees_functions.R
+++ R/CCF_trees_functions.R
@@ -1,192 +1,228 @@
-#
-# # Function that replaces all we need from clonEvol. It creates
-# # for a single sample (CCF values) all possible trees that satisfy the
-# # pigeonhole principle
-# single_sample_CCF_pigeonhole_generator = function(m, cutoff)
-# {
-#   all.trees = NULL
-#
-#   for(i in 1:ncol(m))
-#   {
-#     pio::pioStr("\nAnalyzing sample CCF for", colnames(m)[i], suffix = '\n')
-#
-#     df = m[, i]
-#     names(df) = rownames(m)
-#     df = df[df >= cutoff]
-#
-#     edges = lapply(names(df), function(w) data.frame(from = w, to = names(df[df[w] >= df])))
-#     edges = revolver:::DataFrameToMatrix(Reduce(rbind, edges))
-#     diag(edges) = 0
-#
-#     edges = revolver:::MatrixToDataFrame(edges)
-#     # plot(graph_from_edgelist(as.matrix(edges)))
-#
-#     span_graphs = combn(1:nrow(edges), length(df) - 1)
-#     span_trees = NULL
-#
-#     pb = txtProgressBar(0, ncol(span_graphs), style = 3)
-#
-#     for(j in 1:ncol(span_graphs))
-#     {
-#       setTxtProgressBar(pb, j)
-#
-#       tree = edges[span_graphs[, j], ]
-#       tree = revolver:::DataFrameToMatrix(tree)
-#
-#       # Structural constraint, it should have no
-#       # - multiple roots (disconnected components)
-#       # - no root
-#       # - loops
-#       # - a node with multiple parents
-#
-#       multi_root = sum(apply(tree, 2, function(w) all(w == 0))) > 1
-#       no_root = sum(apply(tree, 2, function(w) all(w == 0))) == 0
-#       is_not_dag = !is.dag(graph_from_adjacency_matrix(tree))
-#       multi_parents = any(colSums(tree) > 1)
-#
-#       if(multi_root | no_root | is_not_dag | multi_parents) next;
-#
-#       # Pigeonhole constraint, it should have no violations of the principle
-#       any_CCF_violation = sapply(rownames(tree), function(w)
-#       {
-#         ccf_node = df[w]
-#
-#         children = revolver:::children(tree, w)
-#         ccf_children = sum(df[children])
-#
-#         ccf_node < ccf_children
-#       })
-#
-#       if(any(any_CCF_violation)) next;
-#
-#       tree = revolver:::MatrixToDataFrame(tree)
-#
-#       span_trees = append(span_trees, list(tree))
-#     }
-#
-#     all.trees = append(all.trees, list(span_trees))
-#   }
-#
-#   names(all.trees) = colnames(m)
-#
-#   pio::pioStr("\nNumber of trees", paste(sapply(all.trees, length), collapse = ', '))
-#
-#   # print(lapply(all.trees, length))
-#
-#   all.trees
-# }
-#
-# # data("Breast.fit")
-# load("../test.revolver/TRACERx-release/TRACERx.cohort.RData", verbose = T)
-#
-# my.data = TRACERx.cohort$phylogenies$CRUK0001[[1]]$dataset
-# sample.groups = TRACERx.cohort$phylogenies$CRUK0001[[1]]$samples_ID
-# my.data2 = my.data
-# my.data[, sample.groups] = my.data[, sample.groups] * 100
-#
-# clonal.cluster = '3'
-#
-# ct = revolver:::clusters.table(my.data2, sample.groups)
-#
-# my.obj = single_sample_CCF_pigeonhole_generator(ct[, sample.groups, drop = FALSE], 0.01)
-#
-# clonevol.obj = revolver:::useClonevo(my.data, sample.groups, clonal.cluster)
-# my.data = revolver:::permuteClusterIds(my.data)
-#
-# capture.output({
-#   clonevol.obj = revolver:::infer.clonal.models(
-#     variants = my.data,
-#     cluster.col.name = 'cluster',
-#     # vaf.col.names = vaf.col.names,
-#     ccf.col.names = sample.groups,
-#     sample.names = sample.groups,
-#     cancer.initiation.model = 'monoclonal',
-#     # cancer.initiation.model = 'polyclonal',
-#     # subclonal.test = 'bootstrap',
-#     subclonal.test = 'none',
-#     subclonal.test.model = 'non-parametric',
-#     # subclonal.test.model = 'beta-binomial',
-#     num.boots = 1000,
-#     founding.cluster = clonal.cluster,
-#     cluster.center = 'median',
-#     ignore.clusters = NULL,
-#     clone.colors = NULL,
-#     min.cluster.vaf = 0.01,
-#     # min probability that CCF(clone) is non-negative
-#     sum.p = 0.05,
-#     # alpha level in confidence interval estimate for CCF(clone)
-#     alpha = 0.05,
-#     verbose = F
-#   )
-# })
-#
-#
-# # Queue functions
-# Q = NULL
-# pop = function(Q){ Q[-1] }
-# push = function(Q,x){ append(Q, list(x)) }
-# peek = function(Q){ Q[[1]] }
-#
-# # Data-query functions
-# s = function(x){ df[df<x] }
-#
-# empty = matrix(0, nrow = length(df) + 1, ncol = length(df) + 1)
-# colnames(empty) = rownames(empty) = c(names(df), "GL")
-#
-# empty["GL", rownames(ct)[ct$is.clonal]] = 1
-#
-# # Init is the clonal node
-# Q = push(NULL, empty)
-#
-# # Loop untill Q is empty
-# repeat{
-#   if(length(Q) == 0) break;
-#
-#   # Get the head of the queue, it's a tree
-#   head_node = peek(Q)
-#
-#   # Remove the head from the queue
-#   Q = pop(Q)
-#
-#   # The head of the queue is a tree, we need to take all of
-#   # its terminal nodes (leaves). We want only leaves with an
-#   # incoming edge, which we get by looking at the transposed
-#   leaves = setdiff(
-#     revolver:::leaves(head_node),
-#     revolver:::leaves(t(head_node)))
-#
-#   # Compute candidates nodes downstream of x
-#   x = df[head_node]
-#   candidates.x = s(x)
-#
-#   # Ensure that the candidates have not been processed
-#   # visited = revolver:::MatrixToDataFrame(head_node)$to
-#   # candidates.x =
-#
-#   # Filter those possible sets of children that
-#   # violate the pigeonhole principle
-#   children = lapply(1:length(candidates.x), function(w) {
-#     com = combn(x = candidates.x, w, simplify = FALSE)
-#     com = com[sapply(com, function(q) sum(q) <=  x)]
-#
-#     lapply(com, function(q) data.frame(from = names(x), to = names(q), stringsAsFactors = FALSE))
-#     # com = lapply(com, function(q) data.frame(from = names(x), to = names(q), stringsAsFactors = FALSE))
-#     # com[sapply(com, function(q) length(q) > 0)]
-#   })
-#
-#   children = children[sapply(children, function(q) length(q) > 0)]
-#   children = Reduce(append, children)
-#
-#   # Concatenate to the current head_node
-#   head_node = revolver:::MatrixToDataFrame(head_node)
-#   children = lapply(children, function(w) rbind(w, head_node))
-#
-#   # Convert to matrix
-#   children = lapply(children, revolver:::DataFrameToMatrix)
-#   for(ch in seq(children)) Q = push(Q, children[[ch]])
-# }
-#
-# combn(candidates.x, 2, simplify = FALSE)
-#
-#
-#
+CCF_phylogeny_univariate = function(x, clonal.cluster)
+{
+  # Compute all posssible branches out of s from x
+  branching = function(
+    n, # node
+    x  # CCF values
+  )
+  {
+    s = which(names(x) == n)
+    y = x[-s]
+
+    size = 1
+    children = NULL
+    repeat {
+      ch = combn(names(y), size)
+      sums = apply(ch, 2, function(w) sum(y[w]))
+
+      ch = ch[, sums <= x[n], drop = FALSE]
+      ch = apply(ch, 2, list)
+
+      # print(ch)
+
+      if(length(ch) == 0) break;
+
+      children = append(children, ch)
+      size = size + 1
+
+      if(size > length(y)) break;
+    }
+
+    children
+  }
+
+  # Empty adjMat for vector of names x, with a fake WL node
+  empty = function(x) {
+
+    m = matrix(0, ncol = length(x), nrow = length(x))
+    colnames(m) = rownames(m) = names(x)
+
+    # WL
+    m = cbind(m, `WL` = 0)
+    m = rbind(m, `WL` = 0)
+
+    # r = names(x)[which.max(x)]
+    r = clonal.cluster
+    m['WL', r] = 1
+
+    m
+  }
+
+  # Queue functions
+  pop = function(Q){ Q[-1] }
+  push = function(Q,x){ append(Q, list(x)) }
+  peek = function(Q){ Q[[1]] }
+
+  # Queue with cache
+  # push.cache = function(Q,x) {
+  # sgn = lapply(Q, as.vector)
+  # sgn = sapply(sgn, paste, collapse = '')
+  #
+  # key = paste(as.vector(x), collapse = '')
+  #
+  # if(!(key %in% sgn)) Q = append(Q, list(x))
+  #
+  # Q
+  # }
+
+  # cache for trees
+  cacheify = function(Q, x) {
+    key = paste(as.vector(x$tree), collapse = '')
+    Q = c(Q, key)
+    Q
+  }
+
+  not_in_cache = function(Q, x) {
+    if(length(Q) == 0) return(TRUE)
+    key = paste(as.vector(x$tree), collapse = '')
+    !(key %in% Q)
+  }
+
+  # push.cache2 = function(Q,x) {
+  #   sgn = lapply(Q, function(w) as.vector(w$tree))
+  #   sgn = sapply(sgn, paste, collapse = '')
+  #
+  #   key = paste(as.vector(x$tree), collapse = '')
+  #
+  #   if(!(key %in% sgn)) Q = append(Q, list(x))
+  #
+  #   Q
+  # }
+
+  # struct to start with is a tree with
+  # r = names(x)[which.max(x)]
+  r = clonal.cluster
+  tree = empty(x)
+
+  init = list(
+    tree = tree,
+    leaves = r,
+    missing = setdiff(names(x), r)
+  )
+
+  # Q is a the queue of partial trees to process
+  # TR are the final set of trees to return
+  # CACHE is the set of substractures that we have already included in Q (those avoid duplicates)
+  Q = TR = CACHE_Q = CACHE_TR = NULL
+
+  Q = push(Q, init)
+  CACHE_Q = cacheify(CACHE_Q, init)
+
+  repeat{
+    if(length(Q) == 0) break;
+
+    # cat("\n\nQ ", length(Q), " cache ",length(CACHE_Q))
+    # cat("\nTR ", length(TR), " cache ",length(CACHE_TR), '\n')
+
+    model = peek(Q)
+    Q = pop(Q)
+
+    # for every leaf node, get all its possible branches,
+    # create corresponding trees and queue them
+    for(n in model$leaves)
+    {
+      # take the list of nodes that are still missing,
+      # and use it to compute the branches
+      missing = x[c(n, model$missing)]
+      branches = branching(n, missing)
+
+      # for every branch,
+      for(br in 1:length(branches))
+      {
+        br = unlist(branches[[br]])
+
+        br.model = model
+        br.model$leaves = c(setdiff(br.model$leaves, n), br)
+        br.model$tree[n, br] = 1
+        br.model$missing = setdiff(br.model$missing, br)
+
+        # if there are no missing nodes, this is a final tree, otherwise it is
+        # a partial and then it goes into Q. In both cases we avoid to include
+        # somehting that we have already analyzed.
+        if(length(br.model$missing) == 0)
+        {
+          if(not_in_cache(CACHE_TR, br.model))
+          {
+            TR = push(TR, br.model$tree)
+            CACHE_TR = cacheify(CACHE_TR, br.model)
+          }
+          # else cat("- TR RJ")
+
+        }
+        else
+        {
+          if(not_in_cache(CACHE_Q, br.model))
+          {
+            Q = push(Q, br.model)
+            CACHE_Q = cacheify(CACHE_Q, br.model)
+          }
+          # else cat("- Q RJ")
+        }
+      }
+    }
+  }
+
+  TR
+}
+
+ClonEvol_surrogate = function(clusters, samples, clonal.cluster, min.CCF = 0.01)
+{
+  stopifnot(all(samples %in% colnames(clusters)))
+  clusters = clusters[, samples, drop = FALSE]
+
+
+  trees = lapply(samples,
+                 function(w) {
+                   regname = w
+
+                   w = clusters[, w]
+                   names(w) = rownames(clusters)
+
+                   # CCF for this sample, we consider only clusters values above 1% CCF
+                   w = w[w > min.CCF]
+
+                   cat("\n Region: ", regname, " - #CCF clusters > 1%: ", length(w))
+
+                   # tree -- default case with no tree
+                   TR = list(data.frame(from = NULL, to = NULL))
+
+                   # Check for consistency of the following parameters
+                   # - the clonal cluster is always the maximum CCF value in all biopsies
+                   # - there are no samples with all CCF below
+                   if(length(w) > 1)
+                   {
+                     # check the clonal cluster
+                     subclonal.clusters = setdiff(names(w), clonal.cluster)
+
+                     if(length(subclonal.clusters) > 0 & !all(w[clonal.cluster] >= w[subclonal.clusters])) {
+                       warning(
+                         '[CORRECTION] The clonal cluster has CCF ', w[clonal.cluster],
+                         ', lower than a subclonal cluster. We have set it to CCF=1 in that sample, but check your CCF estmates...')
+
+                       w[clonal.cluster] = 1
+                     }
+
+                     # trees from this sample, it is a list
+                     TR = CCF_phylogeny_univariate(w, clonal.cluster)
+
+                     # turn them into the format that the tools expects and remove the temporary WL node
+                     TR = lapply(TR, revolver:::MatrixToDataFrame)
+                     TR = lapply(TR, function(y){ y = y[y$from != 'WL', , drop = FALSE] })
+                   }
+                   else {
+                     warning('[SKIP] One region does not have at least 2 CCF clusters above 1%, and will be skipped.')
+                   }
+
+                   TR
+                 }
+  )
+  names(trees) = samples
+
+
+  # just throw an error if no regions have  remove samples with no trees associated, and throw an error if there are no trees at all
+  non_empty_trees = sapply(trees, function(w) { any(lapply(w, nrow) > 0) } )
+
+  if(all(!non_empty_trees)) stop('This patient has no trees, raising an error. Check you CCF estimates ...')
+
+  trees
+}
